1eb2c050e868690ebfd87cc6a1df0b581c9155f3
cristianlj <cristian.lj@hotmail.com>
2019-08-31 18:01:23 -0300

    Interations 1, 2, 3

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,9 +1,75 @@
-/* eslint no-restricted-globals: 'off' */
+// /* eslint no-restricted-globals: 'off' */
+// let movies = [
+//     {
+//         title: 'The Shawshank Redemption',
+//         year: '1994',
+//         director: 'Frank Darabont',
+//         duration: '2h 22min',
+//         genre: ['Crime', 'Drama'],
+//         rate: '8.4'
+//     },
+//     {
+//         title: 'Ahe Godfather','
+//         year: '1972',
+//         director: 'Francis Ford Coppola',
+//         duration: '2h 22min',
+//         genre: ['Crime', 'Drama'],
+//         rate: '8.9'
+//     }];
 
-// Iteration 1: All rates average - Get the average of all rates with 2 decimals 
+// console.log(ratesAverage(movies));
 
- 
-// Iteration 2: Drama movies - Get the average of Drama Movies
+function ratesAverage(arrayMovies) {
+    let total;
+    let rateArray = arrayMovies.reduce((rateArray, movies) => {
+        return rateArray + Number(movies.rate);
+    }, 0)
+
+
+    total = Number((rateArray / arrayMovies.length).toFixed(2));
+
+    if (isNaN(total)) {
+        return 0;
+    }
+
+    return total;
+
+}
+
+function dramaMoviesRate(moviesArray) {
+    let dramaMovies = moviesArray.filter(function (item) {
+        return item.genre.includes("Drama");
+    });
+
+    let total = ratesAverage(dramaMovies);
+
+    return total;
+}
+
+function orderByDuration(moviesArray) {
+
+    return newOrder = moviesArray.sort(function (a, b) {
+        if (a.duration < b.duration) {
+            return -1;
+        }
+        if (a.duration > b.duration) {
+            return 1;
+        }
+        if (a.title < b.title) {
+            return -1;
+        }
+        if (a.title > b.title) {
+            return 1;
+        }
+        return 0;
+    });
+
+   return newOrder; 
+}
+
+console.log(orderByDuration(movies));
+
+
 
 
 // Iteration 3: Ordering by duration - Order by time duration, ascending (in growing order)
